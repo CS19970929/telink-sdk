664c1562b609de6c4e385685a920325f758a10cb
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-08-27 15:28:56 +0800

    refactor(bms): use generic AFE and parameter model

--- tc_ble_single_sdk/vendor/ble_sample/bms_project.h
+++ tc_ble_single_sdk/vendor/ble_sample/bms_project.h
@@ -2,19 +2,19 @@
 #define BMS_PROJECT_H_
 
 #include "tl_common.h"
-#include "sh3673510.h"
+#include "bms_afe.h"
+#include "bms_param.h"
 
-#define BMS_PROTECT_REG_COUNT 65u
+#define BMS_PROTECT_REG_COUNT BMS_PARAM_COUNT
 
 typedef struct {
-    sh3673510_sample_t afe;
+    bms_afe_sample_t afe;
     u16 soc;
     u16 soh;
     u16 capacity_now_x100;
     u16 capacity_full_x100;
     u16 capacity_factory_x100;
     u16 cycle_count;
-    u16 protect[BMS_PROTECT_REG_COUNT];
     u8 mac_public[6];
     u8 afe_init_ok;
     s16 afe_last_error;
@@ -28,6 +28,7 @@ const bms_project_state_t *bms_project_get_state(void);
 u16 bms_project_read_legacy_d000(u16 offset);
 u16 bms_project_read_protect(u16 offset);
 int bms_project_write_protect(u16 offset, u16 value);
+int bms_project_write_protect_block(u16 offset, u16 qty, const u16 *values);
 void bms_project_set_soc(u16 soc);
 int bms_project_command(u16 value);
 
