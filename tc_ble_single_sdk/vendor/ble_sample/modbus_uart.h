88cb5d7cfba75b7326eeef136d15733e5bef9b72
CS19970929 <32015803+CS19970929@users.noreply.github.com>
2026-08-28 14:29:52 +0800

    feat: expose serial low-power diagnostics

--- tc_ble_single_sdk/vendor/ble_sample/modbus_uart.h
+++ tc_ble_single_sdk/vendor/ble_sample/modbus_uart.h
@@ -10,9 +10,20 @@ typedef struct {
     u8 data[MODBUS_UART_DATA_MAX];
 } mb_dma_pkt_t;
 
+typedef struct {
+    u8 active;
+    u8 wake_armed;
+    u8 wake_pending;
+    u8 reserved;
+    u32 sleep_count;
+    u32 wake_count;
+    u32 last_activity_tick;
+} modbus_uart_pm_diag_t;
+
 void modbus_uart_init(void);
 void modbus_uart_irq_proc(void);
 void modbus_uart_process(void);
 void modbus_uart_send(const u8 *data, u32 len);
+void modbus_uart_get_pm_diag(modbus_uart_pm_diag_t *diag);
 
 #endif
